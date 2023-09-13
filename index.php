34b4e9a6b700474d5c27b96c221425a7eca47265
cmassalay <klebcourage@gmail.com>
2023-09-13 20:37:33 +0000

    Made all major sections reusable using php

---
+++ index.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+
+<!--
+ // WEBSITE: https://themefisher.com
+ // TWITTER: https://twitter.com/themefisher
+ // FACEBOOK: https://www.facebook.com/themefisher
+ // GITHUB: https://github.com/themefisher/
+-->
+
+<html class="no-js">
+
+<head>
+  <!-- Basic Page Needs
+        ================================================== -->
+  <meta charset="utf-8">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <link rel="icon" href="favicon.ico">
+  <title>Digital Excel Consultants</title>
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <meta name="author" content="">
+  <!-- Mobile Specific Metas
+        ================================================== -->
+  <meta name="format-detection" content="telephone=no">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- theme meta -->
+  <meta name="theme-name" content="timer" />
+
+
+  <!-- Template CSS Files
+        ================================================== -->
+  <!-- Twitter Bootstrs CSS -->
+  <link rel="stylesheet" href="plugins/bootstrap/bootstrap.min.css">
+  <!-- Ionicons Fonts Css -->
+  <link rel="stylesheet" href="plugins/ionicons/ionicons.min.css">
+  <!-- animate css -->
+  <link rel="stylesheet" href="plugins/animate-css/animate.css">
+  <!-- Hero area slider css-->
+  <link rel="stylesheet" href="plugins/slider/slider.css">
+  <!-- slick slider -->
+  <link rel="stylesheet" href="plugins/slick/slick.css">
+  <!-- Fancybox -->
+  <link rel="stylesheet" href="plugins/facncybox/jquery.fancybox.css">
+  <!-- hover -->
+  <link rel="stylesheet" href="plugins/hover/hover-min.css">
+  <!-- template main css file -->
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+  <!--
+        ==================================================
+        Header Section Start
+        ================================================== -->
+  <?php include 'partials/header.php'; ?>
+
+
+  <!--
+==================================================
+Slider Section Start
+================================================== -->
+  <section id="hero-area">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 text-center">
+          <div class="block wow fadeInUp" data-wow-delay=".3s">
+
+            <!-- Slider -->
+            <section class="cd-intro">
+              <h1 class="wow fadeInUp animated cd-headline slide" data-wow-delay=".4s">
+                <span>HI, MY NAME IS EXCEL CONSULTS &amp; I AM A</span><br>
+                <span class="cd-words-wrapper">
+                  <b class="is-visible">DESIGNER</b>
+                  <b>DEVELOPER</b>
+                  <b>FATHER</b>
+                </span>
+              </h1>
+            </section> <!-- cd-intro -->
+            <!-- /.slider -->
+            <h2 class="wow fadeInUp animated" data-wow-delay=".6s">
+              With 10 years experience, I've occupied many roles including digital design director,<br> web designer and
+              developer. This site showcases some of my work.
+            </h2>
+            <a class="btn-lines dark light wow fadeInUp animated btn btn-default btn-green hvr-bounce-to-right" data-wow-delay=".9s" href="https://themefisher.com/" target="_blank">Download More</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--/#main-slider-->
+
+  <!--
+==================================================
+About Section Start
+================================================== -->
+  <section id="about">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-sm-6">
+          <div class="block wow fadeInLeft" data-wow-delay=".3s" data-wow-duration="500ms">
+            <h2>
+              WELCOME TO DIGITAL EXCEL CONSULTS
+            </h2>
+            <p>
+              Welcome to Excel, where we equip Liberians with the necessary skills and knowledge
+              to embrace technology and contribute to a thriving digital society. We firmly believe i
+              n empowering individuals, bridging the digital divide, and paving the way for a prosperous
+              and inclusive future for all.
+
+            </p>
+          </div>
+        </div>
+        <div class="col-md-6 col-sm-6">
+          <div class="block wow fadeInRight" data-wow-delay=".3s" data-wow-duration="500ms">
+            <img src="images/about/about.jpg" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section> <!-- /#about -->
+
+
+  <!--
+==================================================
+Latest Work Section Start
+================================================== -->
+  <?php include 'partials/latest_work.php'; ?>
+
+  <!--
+==================================================
+Portfolio Section Start
+================================================== -->
+  <section id="feature">
+    <div class="container">
+      <div class="section-heading">
+        <h1 class="title wow fadeInDown" data-wow-delay=".3s">Our Services</h1>
+        <p class="wow fadeInDown" data-wow-delay=".5s">
+          Here at Excel we firmly believe in empowering individuals, bridging the digital divide, and paving the way for a prosperous and inclusive future for all.
+
+        </p>
+      </div>
+      <div class="row">
+        <div class="col-sm-12 col-lg-6">
+          <div class="media wow fadeInUp animated" data-wow-duration="500ms" data-wow-delay="300ms">
+            <div class="media-left">
+              <div class="icon">
+                <i class="ion-code"></i>
+              </div>
+            </div>
+            <div class="media-body">
+              <h4 class="media-heading">Digital Solutions</h4>
+              <p>We develop cutting-edge digital solutions across web, mobile, and desktop platforms.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-lg-6">
+          <div class="media wow fadeInDown animated" data-wow-duration="500ms" data-wow-delay="600ms">
+            <div class="media-left">
+              <div class="icon">
+                <i class="ion-laptop"></i>
+              </div>
+            </div>
+            <div class="media-body">
+              <h4 class="media-heading">Digital Trainings</h4>
+              <p>We offers comprehensive training programs designed to empower individuals and organizations with the necessary digital literacy and IT skills.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-lg-6">
+          <div class="media wow fadeInDown animated" data-wow-duration="500ms" data-wow-delay="900ms">
+            <div class="media-left">
+              <div class="icon">
+                <i class="ion-image"></i>
+              </div>
+            </div>
+            <div class="media-body">
+              <h4 class="media-heading">Multimedia Services</h4>
+              <p>We offer a range of media services, including graphics design, logo design, branding, and multimedia production.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-6">
+          <div class="media wow fadeInDown animated" data-wow-duration="500ms" data-wow-delay="1200ms">
+            <div class="media-left">
+              <div class="icon">
+                <i class="ion-ios-lightbulb-outline"></i>
+              </div>
+            </div>
+            <div class="media-body">
+              <h4 class="media-heading">Consultancy Services</h4>
+              <p>We provide expert guidance and strategic advice to organizations seeking to leverage technology and media for their business growth.</p>
+            </div>
+          </div>
+        </div>
+        <!--      <div class="col-sm-6 col-lg-3">
+        <div class="media wow fadeInDown animated" data-wow-duration="500ms" data-wow-delay="1500ms">
+          <div class="media-left">
+            <div class="icon">
+              <i class="ion-ios-keypad-outline"></i>
+            </div>
+          </div>
+          <div class="media-body">
+            <h4 class="media-heading">Solid Support</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, sint.</p>
+          </div>
+        </div>
+      </div>
+     
+      <div class="col-sm-6 col-lg-4">
+        <div class="media wow fadeInDown animated" data-wow-duration="500ms" data-wow-delay="1800ms">
+          <div class="media-left">
+            <div class="icon">
+              <i class="ion-ios-barcode-outline"></i>
+            </div>
+          </div>
+          <div class="media-body">
+            <h4 class="media-heading">Simple Installation</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, sint.</p>
+          </div>
+        </div>
+      </div> -->
+        <div class="col-lg-12">
+          <div class="text-center">
+            <a class="btn-lines dark light wow fadeInUp animated btn btn-default btn-green hvr-bounce-to-right" href="service.html" target="_blank">Read More</a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </section> <!-- /#feature -->
+
+
+  <!--
+            ==================================================
+            Call To Action Section Start
+            ================================================== -->
+  <?php include 'partials/call_to_action.php'; ?>
+
+  <!--
+            ==================================================
+            Footer Section Start
+            ================================================== -->
+
+
+  <?php require 'partials/footer.php'; ?>
---
+++ partials/footer.php
@@ -0,0 +1,70 @@
+            <!--
+            ==================================================
+            Footer Section Start
+            ================================================== -->
+            <footer id="footer">
+                <div class="container">
+                    <div class="row content-justify-between">
+                        <div class="col-md-8 col-12 text-center text-lg-left text-md-left">
+                          <p class="copyright">&copy; <span id="currentYear"></span> Designed and Developed by <a href="#">Digital Excel Consults</a>.</p>
+
+                        </div>
+                         <!-- Social Media -->
+                         <!--  <div class="col-md-4 col-12">
+                           
+                           <ul class="social text-center text-md-right text-lg-right">
+                                <li>
+                                    <a href="http://wwww.fb.com/themefisher" class="Facebook">
+                                        <i class="ion-social-facebook"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="http://wwww.twitter.com/themefisher" class="Twitter">
+                                        <i class="ion-social-twitter"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="Linkedin">
+                                        <i class="ion-social-linkedin"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="http://wwww.fb.com/themefisher" class="Google Plus">
+                                        <i class="ion-social-googleplus"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div> -->
+
+                    </div>
+                </div>
+            </footer> <!-- /#footer -->
+
+	<!-- Template Javascript Files
+	================================================== -->
+	<!-- jquery -->
+	<script src="plugins/jQurey/jquery.min.js"></script>
+	<!-- Form Validation -->
+    <script src="plugins/form-validation/jquery.form.js"></script> 
+    <script src="plugins/form-validation/jquery.validate.min.js"></script>
+	<!-- slick slider -->
+	<script src="plugins/slick/slick.min.js"></script>
+	<!-- bootstrap js -->
+	<script src="plugins/bootstrap/bootstrap.min.js"></script>
+	<!-- wow js -->
+	<script src="plugins/wow-js/wow.min.js"></script>
+	<!-- slider js -->
+	<script src="plugins/slider/slider.js"></script>
+	<!-- Fancybox -->
+	<script src="plugins/facncybox/jquery.fancybox.js"></script>
+	<!-- template main js -->
+	<script src="js/main.js"></script>
+
+    <script>
+    $(document).ready(function() {
+      var currentYear = new Date().getFullYear();
+      $('#currentYear').text(currentYear);
+    });
+  </script>
+ 	</body>
+</html>
