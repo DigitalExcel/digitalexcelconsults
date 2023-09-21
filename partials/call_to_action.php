5876fd30162a39b77eedc549bddad059fe91776f
cmassalay <klebcourage@gmail.com>
2023-09-21 23:16:18 +0000

    correct contact link the call to action section

--- partials/call_to_action.php
+++ partials/call_to_action.php
@@ -9,7 +9,7 @@
                             <div class="block">
                                 <h2 class="title wow fadeInDown" data-wow-delay=".3s" data-wow-duration="500ms">GET IN TOUCH NOW!</h1>
                                 <p class="wow fadeInDown" data-wow-delay=".5s" data-wow-duration="500ms">Unlock the full potention of technology. Contact us to explore limitless possibilities.</p>
-                                <a href="contact.html" class="btn btn-default btn-contact wow fadeInDown" data-wow-delay=".7s" data-wow-duration="500ms">Contact Us</a>
+                                <a href="contact.php" class="btn btn-default btn-contact wow fadeInDown" data-wow-delay=".7s" data-wow-duration="500ms">Contact Us</a>
                             </div>
                         </div>
                         
