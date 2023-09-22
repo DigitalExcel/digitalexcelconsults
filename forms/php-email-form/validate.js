a1746f9867362ca2e941179eac051c46c26bf266
cmassalay <klebcourage@gmail.com>
2023-09-22 02:48:28 +0000

    troublingshooting email functionality

--- forms/php-email-form/validate.js
+++ forms/php-email-form/validate.js
@@ -60,6 +60,7 @@
       if( response.ok ) {
         return response.text();
       } else {
+        console.log(response);
         throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
       }
     })
--- forms/send_mail.php
+++ forms/send_mail.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <script>alert("Hi this is a test");</script>
-</body>
-</html>
 <?php
 //removes all HTML tags
 $from_email = strip_tags($_POST['email']);
