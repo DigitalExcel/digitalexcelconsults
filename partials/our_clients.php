ed31a0f27abb17628f20716b17d1ba947fd0cdcb
osgun4christ <osgun4christ@gmail.com>
2023-10-05 14:48:51 +0000

    added clients

--- partials/our_clients.php
+++ partials/our_clients.php
@@ -8,14 +8,14 @@
               <div class="col-md-12">
                 <h2 class="subtitle text-center wow fadeInUp animated" data-wow-duration="500ms" data-wow-delay=".3s">Our
                   Happy Clients</h2>
-                <p class="subtitle-des text-center wow fadeInUp animated" data-wow-duration="500ms" data-wow-delay=".5s">Lorem
+                <p class="subtitle-des text-center wow fadeInUp animated" data-wow-duration="500ms" data-wow-delay=".5s">
                   Here are clients we have worked with and we continue to maintain an excellent relationship with:</p>
                 <div id="clients-logo" class="clients-logo-slider">
-                  <img class="img-fluid mx-2" src="images/clients/logo.png" alt="">
-                  <img class="img-fluid mx-2" src="images/clients/bsl.jpg" alt="">
-                  <img class="img-fluid mx-2" src="images/clients/gateway.png" alt="">
-                  <img class="img-fluid mx-2" src="images/clients/logo-4.jpg" alt="">
-                  <img class="img-fluid mx-2" src="images/clients/logo-5.jpg" alt="">
+                  <img class="img-fluid mx-2" src="images/clients/logo.png" width="150" height="100" alt="Living Seed Liberia">
+                  <img class="img-fluid mx-2" src="images/clients/bsl.jpg" width="125" height="100" alt="Bible Society of Liberia">
+                  <img class="img-fluid mx-2" src="images/clients/gateway.png" width="150" height="100" alt="Gateway Medical Clinic">
+                  <img class="img-fluid mx-2" src="images/clients/pricelesslogo.png" width="150" height="100" alt="Priceless REal Estate Services">
+                  <img class="img-fluid mx-2" src="images/clients/tamma.png" width="150" height="100" alt="Tamma Corporation">
                   <img class="img-fluid mx-2" src="images/clients/logo-1.jpg" alt="">
                   <img class="img-fluid mx-2" src="images/clients/logo-2.jpg" alt="">
                   <img class="img-fluid mx-2" src="images/clients/logo-3.jpg" alt="">
