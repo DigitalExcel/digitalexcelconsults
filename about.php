dbf559c5d52238e93a168a1bf29342ad5022b8a3
osgun4christ <osgun4christ@gmail.com>
2023-10-05 15:08:32 +0000

    clients added

--- about.php
+++ about.php
@@ -330,11 +330,11 @@
           <p class="subtitle-des text-center wow fadeInUp animated" data-wow-duration="500ms" data-wow-delay=".5s">Lorem
             Here are clients we have worked with and we continue to maintain an excellent relationship with:</p>
           <div id="clients-logo" class="clients-logo-slider">
-            <img class="img-fluid mx-2" src="images/clients/logo.png" alt="">
-            <img class="img-fluid mx-2" src="images/clients/bsl.jpg" alt="">
-            <img class="img-fluid mx-2" src="images/clients/gateway.png" alt="">
-            <img class="img-fluid mx-2" src="images/clients/logo-4.jpg" alt="">
-            <img class="img-fluid mx-2" src="images/clients/logo-5.jpg" alt="">
+            <img class="img-fluid mx-2" src="images/clients/logo.png" width="150" height="100" alt="Living Seed Team">
+            <img class="img-fluid mx-2" src="images/clients/bsl.jpg" width="120" height="100"alt="Bible Society Liberia">
+            <img class="img-fluid mx-2" src="images/clients/gateway.png" width="150" height="100"alt="Gateway Medical Clinic">
+            <img class="img-fluid mx-2" src="images/clients/pricelesslogo.png" alt="Priceless Real Estate">
+            <img class="img-fluid mx-2" src="images/clients/tamma.png" alt="Tamma Corporation">
             <img class="img-fluid mx-2" src="images/clients/logo-1.jpg" alt="">
             <img class="img-fluid mx-2" src="images/clients/logo-2.jpg" alt="">
             <img class="img-fluid mx-2" src="images/clients/logo-3.jpg" alt="">
