f86f42640ddadc2c8d74fd5ff2f4362a8626f7f8
osgun4christ <osgun4christ@gmail.com>
2023-10-05 15:09:37 +0000

    added size to client logo

--- about.php
+++ about.php
@@ -333,8 +333,8 @@
             <img class="img-fluid mx-2" src="images/clients/logo.png" width="150" height="100" alt="Living Seed Team">
             <img class="img-fluid mx-2" src="images/clients/bsl.jpg" width="120" height="100"alt="Bible Society Liberia">
             <img class="img-fluid mx-2" src="images/clients/gateway.png" width="150" height="100"alt="Gateway Medical Clinic">
-            <img class="img-fluid mx-2" src="images/clients/pricelesslogo.png" alt="Priceless Real Estate">
-            <img class="img-fluid mx-2" src="images/clients/tamma.png" alt="Tamma Corporation">
+            <img class="img-fluid mx-2" src="images/clients/pricelesslogo.png" width="150" height="100"alt="Priceless Real Estate">
+            <img class="img-fluid mx-2" src="images/clients/tamma.png" width="150" height="100"alt="Tamma Corporation">
             <img class="img-fluid mx-2" src="images/clients/logo-1.jpg" alt="">
             <img class="img-fluid mx-2" src="images/clients/logo-2.jpg" alt="">
             <img class="img-fluid mx-2" src="images/clients/logo-3.jpg" alt="">
